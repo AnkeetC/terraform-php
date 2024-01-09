469cbc69e1524d84642bda3c73c079bf351e3747
Ankeet Chauhan <115153160+AnkeetC@users.noreply.github.com>
2024-01-09 15:12:39 +0530

    Update and rename NextJS.sh to NextJS-test.sh

---
+++ NextJS/NextJS-test.sh
@@ -0,0 +1,17 @@
+# Navigate to the home directory
+cd ~
+
+# Clone the Next.js example project
+git clone https://github.com/vercel/next-learn-starter.git
+
+# Navigate to the project directory
+cd next-learn-starter
+
+# Install project dependencies
+npm install
+
+# Build the Next.js project
+npm run build
+
+# Start the Next.js application
+npm start
